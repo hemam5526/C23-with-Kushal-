afa5b9aebb4c837a24bc807e1d781b89a1ce41f5
hemam5526 <72795011+hemam5526@users.noreply.github.com>
2020-11-25 15:21:41 +0530

    Add files via upload

---
+++ Ground.js
@@ -0,0 +1,21 @@
+class Ground
+{
+    constructor()
+    {
+    
+        var options ={
+            isStatic: true
+        }
+    
+       this.body = Bodies.rectangle(200,390,400,20,options);
+       World.add(world,this.body);
+
+    }
+   
+    display()
+    {
+        rectMode(CENTER);
+        rect(this.body.position.x,this.body.position.y,400,20);
+    }
+
+}
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# C23-with-Kushal-
+# bouncyBall
+Bouncy Ball created using matter.js physics engine
